ef8659b026d4ddb2d015dafb514445cbe8ccd580
Christian Vermeulen <info@christianvermeulen.net>
2015-04-11 13:53:26 +0200

    clear repo

--- minecraft-plugins/src/nl/coderdojo/rotterdam/minecraft/advanced/HashMaps.java
+++
@@ -1,7 +0,0 @@
-package nl.coderdojo.rotterdam.minecraft.advanced;
-
-/**
- * Created by Daan on 7-4-2015.
- */
-public class HashMaps {
-}
--- minecraft-plugins/src/nl/coderdojo/rotterdam/minecraft/medium/CustomEvents.java
+++
@@ -1,7 +0,0 @@
-package nl.coderdojo.rotterdam.minecraft.medium;
-
-/**
- * Created by Daan on 7-4-2015.
- */
-public class CustomEvents {
-}
